6e0e2cc6bc7fb503537b0ba4816c4755f8386aca
helwina <morphee3301@hotmail.fr>
2016-04-01 18:08:42 +0200

    ajout d une virgule et d un espace apres chaques elements

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    puts 'fizzbuzz'
+    print 'fizz-buzz' + ', '
   elsif i % 3 == 0
-    puts 'fizz'
+    print 'fizz' + ', '
   elsif i % 5 == 0
-    puts 'buzz'
+    print 'buzz' + ', '
   else
-    puts i
+    print i.to_s + ', '
   end
 end
