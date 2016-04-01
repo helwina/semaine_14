69003d973f34083ed1c14d13f1ee8e4d9d55df5a
helwina <morphee3301@hotmail.fr>
2016-04-01 18:16:02 +0200

    reecriture des print

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print 'fizz-buzz' + ', '
+    print 'fizz-buzz, '
   elsif i % 3 == 0
-    print 'fizz' + ', '
+    print 'fizz, '
   elsif i % 5 == 0
-    print 'buzz' + ', '
+    print 'buzz, '
   else
     print i.to_s + ', '
   end
