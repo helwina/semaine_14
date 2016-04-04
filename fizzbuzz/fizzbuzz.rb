b0af11d05004aad0c0e9149d4cd98a4b81779a23
helwina <morphee3301@hotmail.fr>
2016-04-04 11:56:17 +0200

    ajout des boucles while fir et 101.time

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -9,3 +9,48 @@
     print i.to_s + ', '
   end
 end
+
+=begin
+#on peut egalement ecrire
+101.times do |i|
+  if i == 0
+    print i.to_s + ', '
+  elsif i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
+    print 'fizz-buzz, '
+  elsif i % 3 == 0
+    print 'fizz, '
+  elsif i % 5 == 0
+    print 'buzz, '
+  else
+    print i.to_s + ', '
+  end
+end
+
+#on peut egalement ecrire
+for i in 1..100
+  if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
+    print 'fizz-buzz, '
+  elsif i % 3 == 0
+    print 'fizz, '
+  elsif i % 5 == 0
+    print 'buzz, '
+  else
+    print i.to_s + ', '
+  end
+end
+
+#on peut egalement ecrire
+i = 0
+while i < 100
+  i = i + 1
+  if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
+    print 'fizz-buzz, '
+  elsif i % 3 == 0
+    print 'fizz, '
+  elsif i % 5 == 0
+    print 'buzz, '
+  else
+    print i.to_s + ', '
+  end
+end
+=end
