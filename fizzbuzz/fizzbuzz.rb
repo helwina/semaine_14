31d19d893032ad6a96a1683b4439dbcaf1fa1156
helwina <morphee3301@hotmail.fr>
2016-04-04 12:13:17 +0200

    ajout de l affichage de i avant les fizz buzz et fizz-buzz

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print 'fizz-buzz, '
+    print "#{i} = fizz-buzz, "
   elsif i % 3 == 0
-    print 'fizz, '
+    print "#{i} = fizz, "
   elsif i % 5 == 0
-    print 'buzz, '
+    print "#{i} = buzz, "
   else
     print i.to_s + ', '
   end
@@ -17,11 +17,11 @@ end
   if i == 0
     print i.to_s + ', '
   elsif i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print 'fizz-buzz, '
+    print "#{i} = fizz-buzz, "
   elsif i % 3 == 0
-    print 'fizz, '
+    print "#{i} = fizz, "
   elsif i % 5 == 0
-    print 'buzz, '
+    print "#{i} = buzz, "
   else
     print i.to_s + ', '
   end
@@ -30,11 +30,11 @@ end
 #on peut egalement ecrire
 for i in 1..100
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print 'fizz-buzz, '
+    print "#{i} = fizz-buzz, "
   elsif i % 3 == 0
-    print 'fizz, '
+    print "#{i} = fizz, "
   elsif i % 5 == 0
-    print 'buzz, '
+    print "#{i} = buzz, "
   else
     print i.to_s + ', '
   end
@@ -45,11 +45,11 @@ i = 0
 while i < 100
   i = i + 1
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print 'fizz-buzz, '
+    print "#{i} = fizz-buzz, "
   elsif i % 3 == 0
-    print 'fizz, '
+    print "#{i} = fizz, "
   elsif i % 5 == 0
-    print 'buzz, '
+    print "#{i} = buzz, "
   else
     print i.to_s + ', '
   end
