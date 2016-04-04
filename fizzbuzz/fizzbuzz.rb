d9e8e6678ffb610a7666a50ec8805d237131f8d7
helwina <morphee3301@hotmail.fr>
2016-04-04 12:39:45 +0200

    ajout de chevron <> sur chaque valeur de i pour rendre le print du code plus lisible

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,12 @@
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "#{i} = fizz-buzz, "
+    print "<#{i} fizz-buzz>, "
   elsif i % 3 == 0
-    print "#{i} = fizz, "
+    print "<#{i} fizz>, "
   elsif i % 5 == 0
-    print "#{i} = buzz, "
+    print "<#{i} buzz>, "
   else
-    print i.to_s + ', '
+    print "<#{i}>, "
   end
 end
 
@@ -15,28 +15,28 @@ end
 100.times do |i|
   i = i + 1
   if i == 0
-    print i.to_s + ', '
+    print "<#{i}>, "
   elsif i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "#{i} = fizz-buzz, "
+    print "<#{i} fizz-buzz>, "
   elsif i % 3 == 0
-    print "#{i} = fizz, "
+   print "<#{i} fizz>, "
   elsif i % 5 == 0
-    print "#{i} = buzz, "
+   print "<#{i} buzz>, "
   else
-    print i.to_s + ', '
+    print "<#{i}>, "
   end
 end
 
 #on peut egalement ecrire
 for i in 1..100
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "#{i} = fizz-buzz, "
+    print "<#{i} fizz-buzz>, "
   elsif i % 3 == 0
-    print "#{i} = fizz, "
+    print "<#{i} fizz>, "
   elsif i % 5 == 0
-    print "#{i} = buzz, "
+    print "<#{i} buzz>, "
   else
-    print i.to_s + ', '
+    print "<#{i}>, "
   end
 end
 
@@ -45,13 +45,13 @@ i = 0
 while i < 100
   i = i + 1
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "#{i} = fizz-buzz, "
+    print "<#{i} fizz-buzz>, "
   elsif i % 3 == 0
-    print "#{i} = fizz, "
+    print "<#{i} fizz>, "
   elsif i % 5 == 0
-    print "#{i} = buzz, "
+    print "<#{i} buzz>, "
   else
-    print i.to_s + ', '
+    print "<#{i}>, "
   end
 end
 =end
