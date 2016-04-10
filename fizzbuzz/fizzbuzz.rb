fef6934f76655e61fdd18c99fe4f977ea4829ff2
helwina <morphee3301@hotmail.fr>
2016-04-10 11:26:18 +0200

    supression des ,

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,12 @@
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "<#{i} fizz-buzz>, "
+    print "<#{i} fizz-buzz> "
   elsif i % 3 == 0
-    print "<#{i} fizz>, "
+    print "<#{i} fizz> "
   elsif i % 5 == 0
-    print "<#{i} buzz>, "
+    print "<#{i} buzz> "
   else
-    print "<#{i}>, "
+    print "<#{i}> "
   end
 end
 
@@ -15,26 +15,26 @@ end
 100.times do |i|
   i += 1
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "<#{i} fizz-buzz>, "
+    print "<#{i} fizz-buzz> "
   elsif i % 3 == 0
-   print "<#{i} fizz>, "
+   print "<#{i} fizz> "
   elsif i % 5 == 0
-   print "<#{i} buzz>, "
+   print "<#{i} buzz> "
   else
-    print "<#{i}>, "
+    print "<#{i}> "
   end
 end
 
 #on peut egalement ecrire
 for i in 1..100
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "<#{i} fizz-buzz>, "
+    print "<#{i} fizz-buzz> "
   elsif i % 3 == 0
-    print "<#{i} fizz>, "
+    print "<#{i} fizz> "
   elsif i % 5 == 0
-    print "<#{i} buzz>, "
+    print "<#{i} buzz> "
   else
-    print "<#{i}>, "
+    print "<#{i}> "
   end
 end
 
@@ -43,13 +43,13 @@ i = 0
 while i < 100
   i += 1
   if i % 3 == 0 && i % 5 == 0 #on peut egalement utiliser  if i % 15 == 0
-    print "<#{i} fizz-buzz>, "
+    print "<#{i} fizz-buzz> "
   elsif i % 3 == 0
-    print "<#{i} fizz>, "
+    print "<#{i} fizz> "
   elsif i % 5 == 0
-    print "<#{i} buzz>, "
+    print "<#{i} buzz> "
   else
-    print "<#{i}>, "
+    print "<#{i}> "
   end
 end
 =end
